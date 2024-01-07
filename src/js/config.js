c733ad5f137c8239520b52c6ccff52b4b82f8705
RuhelShaikh <ruhelshaikh786@gmail.com>
2024-01-07 12:40:04 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e1d81983-d1d1-480e-846f-213f3a5f73f1';
+export const MODAL_CLOSE_SEC = 2.5;
