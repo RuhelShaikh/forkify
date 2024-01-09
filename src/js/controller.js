bc01c6b7a3ead824e5724aa81042166e816404de
RuhelShaikh <ruhelshaikh786@gmail.com>
2024-01-09 08:51:20 +0530

    faltu

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,6 +11,8 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
+console.log('hi');
+
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
