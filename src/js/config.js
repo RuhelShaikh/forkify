5f33966c49f0d804bace88ff1f563fc0b043d872
RuhelShaikh <ruhelshaikh786@gmail.com>
2024-01-14 13:21:10 +0530

    I dunno

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e1d81983-d1d1-480e-846f-213f3a5f73f1';
+export const MODAL_CLOSE_SEC = 2.5;
+export const recipeId = '65a36b08eec55400146c11b9';
